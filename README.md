5237953722cd051c8c938f51357e735bea157468
Ivan Paulovich <ivan@paulovich.net>
2019-03-03 19:51:56 +0100

    Readme

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# todolist-clean-architecture
-Another Clean Architecture example based on Uncle Bob book
+# Clean Architecture Implementation of Todo List app
+
+An example of the classic Clean Architecture from Uncle Bob.
+
+# :zap: Running Unit Tests
+
+```
+dotnet test tests/TodoList.UnitTests/TodoList.UnitTests.csproj
+```
+
+## :checkered_flag: Developer Environment
+
+* MacOS Sierra
+* VSCode :heart:
+* [.NET SDK 2.2](https://www.microsoft.com/net/download/dotnet-core/2.2).
+
+## :telephone: For Support and Issues
+
+I am happy to be clarify the implementation through the **Issues Tab**.
--- tests/TodoList.UnitTests/UpdateTitleTests.cs
+++ tests/TodoList.UnitTests/UpdateTitleTests.cs
@@ -3,12 +3,48 @@ namespace TodoList.UnitTests
     using System;
     using Xunit;
     using TodoList.Core;
-    using TodoList.Core.UseCases.AddTask;
+    using TodoList.Core.UseCases.UpdateTitle;
     using TodoList.Core.UseCases;
     using Moq;
-    
+
     public sealed class UpdateTitleTests
     {
-        
+        [Fact]
+        public void GivenNullInput_ThrowsException()
+        {
+            IUseCase<Input> addTask = new Interactor();
+
+            Assert.Throws<Exception>(() => addTask.Execute(null));
+        }
+
+        [Fact]
+        public void GivenNullTitle_ThrowsException()
+        {
+            InputBuilder builder = new InputBuilder();
+            IUseCase<Input> addTask = new Interactor();
+
+            Assert.Throws<Exception>(() => addTask.Execute(builder.Build()));
+        }
+
+        [Fact]
+        public void GivenEmptyTitle_ThrowsException()
+        {
+            InputBuilder builder = new InputBuilder();
+            builder.WithTitle(string.Empty);
+            IUseCase<Input> addTask = new Interactor();
+
+            Assert.Throws<Exception>(() => addTask.Execute(builder.Build()));
+        }
+
+        [Fact]
+        public void GivenNotNullTitle_DoesNotThrowException()
+        {
+            InputBuilder builder = new InputBuilder();
+            builder.WithTitle("My Title");
+            IUseCase<Input> addTask = new Interactor();
+
+            Exception ex = Record.Exception(() => addTask.Execute(builder.Build()));
+            Assert.Null(ex);
+        }
     }
 }
